ff6bfa39e6980e9173c32bde65c5d84557d66676
JeongMin Lee <jmboc.0501@gmail.com>
2022-03-07 17:47:18 +0900

    카테고리 및 태그 메뉴와 설정 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "첫 포스팅 입니다.",
-        "excerpt":"오늘 블로그 처음 개설   잘 부탁드려요!  ","categories": [],
-        "tags": [],
-        "url": "/first/",
+        "excerpt":"오늘 블로그 처음 개설   잘 부탁드려요!   &gt;&gt;&gt; type(4.5) &lt;class 'float'&gt;  &gt;&gt;&gt; type(5.8) &lt;class 'float'&gt;  &gt;&gt;&gt; type(2342423424.3) &lt;class 'float'&gt;  &gt;&gt;&gt; type(4.0) &lt;class 'float'&gt;  &gt;&gt;&gt; type(0.0) &lt;class 'float'&gt;  &gt;&gt;&gt; type(-23.5) &lt;class 'float'&gt;  ","categories": ["Typescript"],
+        "tags": ["typescript","reactjs"],
+        "url": "/typescript/first/",
         "teaser": null
       }]
