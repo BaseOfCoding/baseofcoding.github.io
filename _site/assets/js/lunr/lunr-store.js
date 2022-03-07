13751ef196e52b24b5eef568eb279127695026d8
JeongMin Lee <jmboc.0501@gmail.com>
2022-03-07 16:59:53 +0900

    댓글 기능 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "첫 포스팅 입니다.",
+        "excerpt":"오늘 블로그 처음 개설   잘 부탁드려요!  ","categories": [],
+        "tags": [],
+        "url": "/first/",
+        "teaser": null
+      }]
